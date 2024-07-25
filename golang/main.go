26566b5d9e31769e931acd9aac41dd42e4b6b1a3
AlyHKafoury <aly.kafoury@gmail.com>
2024-07-26 00:34:30 +0300

    add network partition command

--- golang/main.go
+++ golang/main.go
@@ -5,15 +5,21 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 
+	"github.com/google/uuid"
+	v3 "github.com/projectcalico/api/pkg/apis/projectcalico/v3"
+	"github.com/projectcalico/api/pkg/client/clientset_generated/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 type Server struct {
-	K8sClientSet *kubernetes.Clientset
+	K8sClientSet    *kubernetes.Clientset
+	CalicoClientSet *clientset.Clientset
 }
 
 type DeploymentInfo struct {
@@ -23,8 +29,18 @@ type DeploymentInfo struct {
 }
 
 type ClusterDeploymentsInfo struct {
-	ReadyDeployments  []DeploymentInfo `json:ready_deployments`
-	FailedDeployments []DeploymentInfo `json:failed_deployments`
+	ReadyDeployments  []DeploymentInfo `json:"ready_deployments"`
+	FailedDeployments []DeploymentInfo `json:"failed_deployments"`
+}
+
+type DenyNetworkRequestWorkload struct {
+	Namespace string            `json:"namespace"`
+	Labels    map[string]string `json:"labels"`
+}
+
+type DenyNetworkRequest struct {
+	A DenyNetworkRequestWorkload `json:"workload_a"`
+	B DenyNetworkRequestWorkload `json:"workload_b"`
 }
 
 func main() {
@@ -38,19 +54,25 @@ func main() {
 		panic(err)
 	}
 
-	clientset, err := kubernetes.NewForConfig(kConfig)
+	clientsetVanilla, err := kubernetes.NewForConfig(kConfig)
 	if err != nil {
 		panic(err)
 	}
 
-	version, err := getKubernetesVersion(clientset)
+	clientsetCalico, err := clientset.NewForConfig(kConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	version, err := getKubernetesVersion(clientsetVanilla)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("Connected to Kubernetes %s\n", version)
 	server := Server{
-		K8sClientSet: clientset,
+		K8sClientSet:    clientsetVanilla,
+		CalicoClientSet: clientsetCalico,
 	}
 	//getDeploymentsHealth(clientset)
 	if err := startServer(*listenAddr, server); err != nil {
@@ -75,7 +97,8 @@ func getKubernetesVersion(clientset kubernetes.Interface) (string, error) {
 // Expects a listenAddr to bind to.
 func startServer(listenAddr string, server Server) error {
 	http.HandleFunc("/healthz", healthHandler)
-	http.HandleFunc("/clusterdeploymentsinfo", server.clusterDeploymentsInfo)
+	http.HandleFunc("/clusterdeploymentsinfo", server.clusterDeploymentsInfoHandler)
+	http.HandleFunc("/denyNetworkPolicy", server.denyNetworkPolicyHandler)
 
 	fmt.Printf("Server listening on %s\n", listenAddr)
 
@@ -93,17 +116,15 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // Cluster Deployments Info returns the status of each deployment of the cluster
-func (s *Server) clusterDeploymentsInfo(w http.ResponseWriter, r *http.Request) {
+func (s *Server) clusterDeploymentsInfoHandler(w http.ResponseWriter, r *http.Request) {
 
 	clusterDeploymentsInfo, err := getDeploymentsHealth(s.K8sClientSet)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, err := w.Write([]byte("Internal Server Error"))
-		if err != nil {
-			fmt.Println("failed writing to response")
-		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
 
 	err = json.NewEncoder(w).Encode(clusterDeploymentsInfo)
 	if err != nil {
@@ -117,19 +138,18 @@ func getDeploymentsHealth(clientset kubernetes.Interface) (*ClusterDeploymentsIn
 	deploymentsClient := clientset.AppsV1().Deployments(metav1.NamespaceAll)
 	deployments, err := deploymentsClient.List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
-		panic(err.Error())
+		return nil, err
 	}
 
 	clusterInfo := new(ClusterDeploymentsInfo)
 
-	fmt.Printf("Found %d deployments:\n", len(deployments.Items))
 	for _, deployment := range deployments.Items {
 		currentDeploymentInfo := DeploymentInfo{
 			Name:          deployment.Name,
 			RequestedPods: *deployment.Spec.Replicas,
 			ReadyPods:     deployment.Status.ReadyReplicas,
 		}
-		fmt.Printf("%+v\n", currentDeploymentInfo)
+
 		if *deployment.Spec.Replicas <= deployment.Status.ReadyReplicas {
 			clusterInfo.ReadyDeployments = append(clusterInfo.ReadyDeployments, currentDeploymentInfo)
 		} else {
@@ -138,4 +158,96 @@ func getDeploymentsHealth(clientset kubernetes.Interface) (*ClusterDeploymentsIn
 	}
 	fmt.Printf("%+v\n", clusterInfo)
 	return clusterInfo, nil
+}
+
+// Handler to create deny policy based on post request
+func (s *Server) denyNetworkPolicyHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the request method is POST
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	defer r.Body.Close()
+
+	var denyNetworkRequest DenyNetworkRequest
+	err = json.Unmarshal(body, &denyNetworkRequest)
+	if err != nil {
+		http.Error(w, "Error parsing JSON", http.StatusBadRequest)
+		return
+	}
+
+	n, err := createDenyNetworkPolicy(s.K8sClientSet, *s.CalicoClientSet, denyNetworkRequest)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write([]byte(n))
+	if err != nil {
+		fmt.Println("failed writing to response")
+	}
+}
+
+// Render string map to Calico selector string
+func renderMap(m map[string]string) string {
+	var sb strings.Builder
+	first := true
+	for key, value := range m {
+		if !first {
+			sb.WriteString(" && ")
+		}
+		sb.WriteString(fmt.Sprintf("%s == '%s'", key, value))
+		first = false
+	}
+	return sb.String()
+}
+
+// Creates Network Policy to stop connections between two workloads by label and namespace
+func createDenyNetworkPolicy(clientset kubernetes.Interface, calicoClientset clientset.Clientset, requestdetails DenyNetworkRequest) (string, error) {
+	namespaceB, err := clientset.CoreV1().Namespaces().Get(context.TODO(), requestdetails.B.Namespace, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println(renderMap(requestdetails.B.Labels))
+	fmt.Println(renderMap(namespaceB.Labels))
+	networkPolicy := &v3.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("deny-network-policy-%s", uuid.New().String()),
+			Namespace: requestdetails.A.Namespace,
+		},
+		Spec: v3.NetworkPolicySpec{
+			Selector: renderMap(requestdetails.A.Labels),
+			Ingress: []v3.Rule{{
+				Action: v3.Deny,
+				Source: v3.EntityRule{
+					Selector:          renderMap(requestdetails.B.Labels),
+					NamespaceSelector: renderMap(namespaceB.Labels),
+				},
+			}},
+			Egress: []v3.Rule{{
+				Action: v3.Deny,
+				Destination: v3.EntityRule{
+					Selector:          renderMap(requestdetails.B.Labels),
+					NamespaceSelector: renderMap(namespaceB.Labels),
+				},
+			}},
+		},
+	}
+
+	n, err := calicoClientset.ProjectcalicoV3().NetworkPolicies(requestdetails.A.Namespace).Create(context.TODO(), networkPolicy, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Println("Error in here" + err.Error())
+		return "", err
+	}
+
+	fmt.Println("NetworkPolicy created with name:", networkPolicy.Name)
+	return n.Name, nil
 }
